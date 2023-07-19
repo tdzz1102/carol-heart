a34e89acaa057c33615650d414c9cfa050c89a62
tdzz1102 <wasuremono127@gmail.com>
2023-07-19 16:37:53 +0800

    bv, name -> ok to play

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # carol-heart
 
 bv -> navidrome music
+
+
+```py
+import ffmpeg
+
+def extract_audio_from_m4s(m4s_file, output_file):
+    (
+        ffmpeg
+        .input(m4s_file) # can be bytes
+        .output(output_file, acodec='copy') # must be filename
+        .run()
+    )
+
+m4s_file = 'path/to/your/file.m4s'
+output_file = 'path/to/output/audio.m4a'
+
+extract_audio_from_m4s(m4s_file, output_file)
+```
--- main.py
+++ main.py
@@ -1,16 +1,110 @@
-# This is a sample Python script.
+import datetime as dt
+import requests
+import ffmpeg
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from pathlib import Path
+from pydantic import BaseModel
+from mutagen.mp4 import MP4, MP4Cover
 
 
-# Press the green button in the gutter to run the script.
+MUSIC_PATH = Path(__file__).parent / 'Data' / 'music'
+TMP_PATH = Path(__file__).parent / 'tmp'
+
+
+class Song(BaseModel):
+    bv: str
+    name: str
+    album_name = 'Carol'
+    atrist = '珈乐'
+    
+    # 後から取得
+    cover_url: str | None
+    audio_url: str | None # m4s format, need transform
+    date: dt.date | None
+
+
+def get_save_path(song: Song) -> str:
+    filename = f"{song.date.strftime('%Y-%m-%d')}-{song.name}.m4a"
+    return str(MUSIC_PATH / filename)
+
+
+def get_tmp_path(song: Song) -> str:
+    return str(TMP_PATH / song.bv)
+
+
+def get_song_from_bv(bv, name) -> Song:
+    # bilibili apiを使い、情報を取得
+    info_data: dict = requests.get(f'https://api.bilibili.com/x/web-interface/view?bvid={bv}').json()['data']
+    song = Song(bv=bv, name=name)
+    pub_timestamp = int(info_data.get('pubdate'))
+    song.date = dt.date.fromtimestamp(pub_timestamp)
+    song.cover_url = info_data.get('pic')
+
+    play_data: dict = requests.get(f"http://api.bilibili.com/x/player/playurl?fnval=16&bvid={bv}&cid={info_data['cid']}", allow_redirects=True).json()['data']
+    song.audio_url = play_data['dash']['audio'][0]['baseUrl']
+    print(song)
+    return song
+
+
+def handle_exception(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            print(f"例外が発生しました: {str(e)}")
+    return wrapper
+
+
+@handle_exception
+def pull_song(song: Song) -> None:
+    # m4aファイルを取得
+    headers = {
+        "User-Agent": "Mozilla/5.0` (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Range": "bytes=0-",
+        "Referer": f"https://api.bilibili.com/x/web-interface/view?bvid={song.bv}",
+        "Origin": "https://www.bilibili.com",
+        "Connection": "keep-alive"
+    }
+    # res = requests.get(song.audio_url, headers=headers, allow_redirects=True)
+    # print(res.status_code)
+    m4sb = requests.get(song.audio_url, headers=headers, allow_redirects=True).content
+    tmp_path = get_tmp_path(song)
+    m4a_path = get_save_path(song)
+    with open(tmp_path, 'wb') as f:
+        f.write(m4sb)
+    ffmpeg.input(tmp_path).output(m4a_path).run()
+    m4a = MP4(m4a_path)
+
+    # cover
+    try:
+        coverb = requests.get(song.cover_url).content
+        m4a["covr"] = [MP4Cover(coverb, imageformat=MP4Cover.FORMAT_PNG if song.cover_url.endswith(
+            'png') else MP4Cover.FORMAT_JPEG)]
+    except Exception as e:
+        # logger.warning(f'No cover supplyed. The error message is {e}')
+        pass
+
+    # date
+    m4a.tags["\xa9day"] = [song.date.strftime("%Y-%m-%d")]
+
+    # album title
+    m4a.tags["\xa9alb"] = [song.album_name] 
+    
+    # name
+    m4a.tags['\xa9nam'] = [song.name]
+
+    # artist
+    m4a.tags['\xa9ART'] = [song.atrist]
+
+    m4a.save()
+        
+        
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        bv, name = input('bv: '), input('name: ')
+        song = get_song_from_bv(bv, name)
+        pull_song(song)
+        print('ok, next')
